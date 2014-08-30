d228986f95ebff5a55d35fb36adcd15c5bc405b0
anish <anish@anish-PC>
2014-08-30 22:58:20 +0100

    3rd

--- GitDemo/src/com/example/gitdemo/MainActivity.java
+++ GitDemo/src/com/example/gitdemo/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 
 public class MainActivity extends Activity {
 
-	Button btnTest, btnTest2, btnTest3, btnTest4;
+	Button btnTest, btnTest2, btnTest3, btnTest4, btnTest5;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
