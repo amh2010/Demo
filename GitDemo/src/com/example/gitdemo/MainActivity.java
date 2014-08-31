5de2de6d79c3920187a5163afdb009a84b1c9117
amh2010 <krishnananish422@gmail.com>
2014-08-31 12:03:48 +0100

    Added buttonClick functionality

--- GitDemo/src/com/example/gitdemo/MainActivity.java
+++ GitDemo/src/com/example/gitdemo/MainActivity.java
@@ -3,15 +3,30 @@ package com.example.gitdemo;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	Button btnTest, btnTest2, btnTest3, btnTest4, btnTest5, btnTest6;
+	TextView txtView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		btnTest = (Button)findViewById(R.id.button1);
+		txtView = (TextView)findViewById(R.id.textView1);
+		btnTest.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				txtView.setText("Hello Anish");
+				
+			}
+		});
 	}
 
 	@Override
